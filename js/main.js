0839735c68669ecca8c8ccbc484eedf252a63285
David Sembroiz <david.sembroiz@gmail.com>
2017-09-06 10:35:33 +0200

    fixed text link

--- js/main.js
+++ js/main.js
@@ -61,11 +61,6 @@ function initialize() {
 	  });
 };
 
-function isValidEmailAddress(emailAddress) {
-	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-	return pattern.test(emailAddress);
-};
-
 function changeNavbarColor() {
 	var navbar = $('.navbar');
 	var navHeight = navbar.height();
@@ -94,61 +89,6 @@ function collapseNavbar() {
 		}
 		else {
 			navbar.removeClass('custom-collapse');
-		}
-	});
-};
-
-function validateForm() {
-	$("#contact-form").validate({
-		rules: {
-			cname: {
-				required: true
-			},
-			cmail: {
-				required: true,
-				email: true
-			},
-			cmess: {
-				required: true
-			}
-		},
-
-		messages: {
-			cname: {
-				required: "You must enter a name"
-			},
-			cmail: {
-				required: "You must enter an e-mail",
-				email: "Enter a valid e-mail"
-			},
-			cmess: {
-				required: "You must enter a message"
-			}
-		},
-
-		submitHandler: function(form) {
-			$(form).ajaxSubmit({
-				type: "POST",
-				url: "contactForm.php",
-				data: $(form).serialize(),
-				beforeSend: function() {
-							$('#contact-form button').empty();
-							$('#contact-form button').removeClass('btn-success');
-							$('#contact-form button').addClass('btn-warning');
-							$('#contact-form button').append('<i class="fa fa-cog fa-spin"></i> Wait...');
-						},
-				success: function(response) {
-
-					$('#contact-form').fadeOut(200);
-					$('.ajax-response').html("Thank you for contacting!");
-					$('.ajax-response').fadeIn(500);
-					console.log("success");
-				},
-	                    
-	            error: function() {
-	                   console.log("error"); 
-	            }
-			})
 		}
 	});
 };
